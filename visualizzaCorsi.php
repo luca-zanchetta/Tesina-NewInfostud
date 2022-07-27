d81c3daf66a436b931ecca0e268b8691d7a189b0
Luca Zanchetta <102416762+luca-zanchetta@users.noreply.github.com>
2022-07-27 11:28:07 +0200

    Adattamento stile visualizzazione
    Adattamento stile di visualizzazione dei corsi di laurea, dei corsi e dei docenti; è stato reso simile allo stile di visualizzazione delle bacheche.

---
+++ visualizzaCorsi.php
@@ -0,0 +1,162 @@
+<?php
+require_once('../Sito/phpFunctions.php');
+?>
+
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+    <link rel="stylesheet" href="stile-base.css">
+    <link rel="stylesheet" href="stileVisualizzazioneLista.css">
+    <title>Corsi di Laurea - Infostud</title>
+</head>
+<body style="background-color: gainsboro;">
+    <div class="header">
+        <div class="nav-left">
+            <div class="nav-logo">
+                <a href="homepage.php">
+                    <img src="https://store-images.s-microsoft.com/image/apps.51215.9007199266623456.05e3a154-d5ac-49d8-af6e-ab2f789dc26d.f443b25b-1668-48aa-8137-f8e5609aee45?mode=scale&q=90&h=300&w=300" alt="logo" width="90px">
+                </a>
+            </div>
+            <div class="vertical-bar"></div>
+            <h2>
+                <form action="">
+                    <input type="button">
+                </form>
+                Infostud
+            </h2>  
+        </div>
+        <div class="nav-central">
+            <form action="visualizzaCorsi.php" method="POST">
+                <div class="nav-logo">
+                    <input type="submit" name="ricerca" value="">
+                    <img src="search.png" alt="err" width="20px" style="display: inline-flex;">
+                </div>    
+                    <input type="text" name="filtro">              
+            </form>
+        </div>
+        <div class="nav-right">
+            <h2>
+                <form action="login.php" method="POST">
+                    <input type="submit" value="">
+                    <input name="loginType" type="hidden" value="Studente">
+                </form>
+                    Studenti      
+            </h2>
+            <div class="vertical-bar"></div>
+            <h2>
+                <form action="login.php" method="POST">
+                    <input type="submit" value="">
+                    <input name="loginType" type="hidden" value="Docente">
+                </form>
+                    Docenti
+                
+            </h2>
+            <div class="vertical-bar"></div>
+            <h2>
+                <form action="login.php" method="POST">
+                    <input type="submit" value="">
+                    <input name="loginType" type="hidden" value="Segretario">
+                </form>
+                    Segreteria
+                
+            </h2>
+            <div class="vertical-bar"></div>
+            <h2>
+                <form action="login.php" method="POST">
+                    <input type="submit" value="">
+                    <input name="loginType" type="hidden" value="Amministratore">
+                </form>
+                    Amministrazione
+            </h2>   
+        </div>
+    </div>
+    <div class="central-block">
+        <div class="sidebar">
+            <h5>
+                <a class="opzione" href="homepage.php">Homepage</a>
+            </h5>
+            <h5>
+                <a class="opzione" href="visualizzaCorsiDiLaurea.php">I nostri corsi di laurea</a>
+            </h5>
+            <h5>
+                <a class="opzione" href="visualizzaDocenti.php">I nostri docenti</a>
+            </h5>
+        </div>
+        <div class="body">
+            <h2 style="margin-left: 2.5%; font-size: 200%;">I NOSTRI CORSI:</h2>
+            <hr class="redBar" />
+            <br />
+            <div class="listContainer">
+                <div class="listItem">
+                    <div class="element">
+                        <h2>Corso</h2>
+                    </div>
+                    <div class="element">
+                        <h2>Corso di Laurea</h2>
+                    </div>
+                </div>
+                <hr />
+            <?php
+                $corsi = [];
+                if(isset($_POST['filtro']) && $_POST['filtro'] != "") {
+                    $corsi = getCorsiLike($_POST['filtro']);
+
+                    if(!$corsi) {
+                        echo "<h3 class=\"voceElenco\">Nessun corso corrispondente ai criteri di ricerca.</h3>";
+                    }
+                    else {
+                        foreach($corsi as $corso) {
+                            $nomeCorsoDiLaurea = getNomeCorsoDiLaureaByID($corso->id_corso_laurea);
+                        ?>
+                            <div class="listItem">
+                                <div class="element">
+                                    <h2><?php echo $corso->nome; ?></h2>
+                                </div>
+                                <div class="element">
+                                    <h2><?php echo $nomeCorsoDiLaurea; ?></h2>
+                                </div>
+                                <div class="lastElement">
+                                    <a href="fittizia.php"><img class="arrow" width="30px" height="30px" alt="err" src="arrowBlack.png"></a>
+                                </div>
+                            </div>
+                            <hr />
+                        <?php
+                        }
+                    }
+                }
+                else {
+                    $corsi = getCorsi();
+
+                    if(!$corsi) {
+                        echo "<h3 class=\"voceElenco\">Al momento non sono disponibili corsi.</h3>";
+                    }
+                    else {
+                        foreach($corsi as $corso) {
+                            $nomeCorsoDiLaurea = getNomeCorsoDiLaureaByID($corso->id_corso_laurea);
+                        ?>
+                            <div class="listItem">
+                                <div class="element">
+                                    <h2><?php echo $corso->nome; ?></h2>
+                                </div>
+                                <div class="element">
+                                    <h2><?php echo $nomeCorsoDiLaurea; ?></h2>
+                                </div>
+                                <div class="lastElement">
+                                    <a href="fittizia.php"><img class="arrow" width="30px" height="30px" alt="err" src="arrowBlack.png"></a>
+                                </div>
+                            </div>
+                            <hr />
+                        <?php
+                        }
+                    }
+                }
+            ?>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
