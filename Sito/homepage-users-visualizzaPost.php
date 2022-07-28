c4651fd91af8767f3263a88498a5326a1d507ffd
Mattia Aquilina <64703986+Orioncode0@users.noreply.github.com>
2022-07-28 18:41:37 +0200

    Creazione pagina visualizzaPost

---
+++ Sito/homepage-users-visualizzaPost.php
@@ -0,0 +1,262 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+    <link rel="stylesheet" href="stile-base.css">
+    <link rel="stylesheet" href="stileHomepage-users.css">
+    <link rel="stylesheet" href="stileBacheca.css">
+    <link rel="stylesheet" href="stilePost.css">
+    <title>Homepage</title>
+</head>
+<body>
+    <div class="header">
+        <div class="nav-left">
+            <div class="nav-logo">
+                <a href="homepage.php">
+                    <img src="https://store-images.s-microsoft.com/image/apps.51215.9007199266623456.05e3a154-d5ac-49d8-af6e-ab2f789dc26d.f443b25b-1668-48aa-8137-f8e5609aee45?mode=scale&q=90&h=300&w=300" alt="logo" width="90px">
+                </a>
+            </div>
+            <div class="vertical-bar"></div>
+                <h2>
+                    <form action="">
+                        <input type="button">
+                    </form>
+                    Infostud
+                </h2>
+            <div class="vertical-bar"></div>
+        </div>
+        <div class="nav-right">
+        <h2>
+            <form action="">
+                <input type="button">
+            </form>
+                Logout
+        </h2>
+        </div>
+    </div>
+    <div class="central-block">
+        <div class="sidebar">
+            <h5>
+                Informazioni
+            </h5>
+            <div style="display:flex;">
+                <img src="arrow.png" alt="duce" width="20px" height="20px" style="display:flex;align-content:center">
+                <h5 style="display: flex;margin:0px;">
+                    <a class="opzionetab" href="homepage-users-Anagrafica.php" style="display: flex;margin:0px;">Visualizza anagrafica</a>
+                </h5>
+            </div>
+            <div style="display:flex;">
+                <img src="arrow.png" alt="duce" width="20px" height="20px" style="display:flex;align-content:center">
+                <h5 style="display: flex;margin:0px;">
+                    <a class="opzionetab" href="homepage-users-Carriera.php" style="display: flex;margin:0px;">Visualizza carriera</a>
+                </h5>
+            </div>
+            <h5>
+                Esami
+            </h5>
+            <div style="display:flex;">
+                <img src="arrow.png" alt="duce" width="20px" height="20px" style="display:flex;align-content:center">
+                <h5 style="display: flex;margin:0px;">
+                    <a class="opzionetab" href="homepage-users-appelliPrenotati.php" style="display: flex;margin:0px;">Appelli prenotati</a>
+                </h5>
+            </div>
+            <div style="display:flex;">
+                <img src="arrow.png" alt="duce" width="20px" height="20px" style="display:flex;align-content:center">
+                <h5 style="display: flex;margin:0px;">
+                    <a class="opzionetab" href="homepage-users-prenotaAppello.php" style="display: flex;margin:0px;">Prenota Appello</a>
+                </h5>
+            </div>
+            <div style="display:flex;">
+                <img src="arrow.png" alt="duce" width="20px" height="20px" style="display:flex;align-content:center">
+                <h5 style="display: flex;margin:0px;">
+                    <a class="opzionetab" href="homepage-users-esamiSostenuti.php" style="display: flex;margin:0px;">Esami Sostenuti</a>
+                </h5>
+            </div>
+            <h5 style="display: flex;margin:0px;">
+                    <a class="opzionetab" href="homepage-bacheca.php" style="display: flex;margin:0px;">Bacheca</a>
+            </h5>
+            <h5 style="display: flex;margin:0px;">
+                    <a class="opzionetab" href="homepage-faq.php" style="display: flex;margin:0px;">FAQ</a>
+            </h5>
+            
+        </div>
+        <div class="body">
+            <div class="infoTitle">
+                <div class="infoTitle-position">
+                    <h2>Home > Bacheca > Basi di Dati</h2><!--Generato dallo script-->
+                </div>
+                <div class="infoTitle-user">
+                    <h2>Nome,Cognome, Matricola</h2><!--Generato dallo script-->
+                </div>
+            </div>    
+            <hr />
+            <div class="postInfo">
+                <div class="postTitle">
+                    Cerco appunti!
+                </div>
+                <div class="postData">
+                    <div class="postAuthor">
+                        Francesco, Totti, 1923483
+                    </div>
+                    <div class="postDate">
+                       19/02/2020
+                    </div>
+                </div>
+            </div>
+            <div class="pageNav">
+                <div class="prev">
+                    Prev  
+                </div>
+                <div class="pageList">
+                    <div class="pageNumber">
+                        1
+                    </div>
+                    <div class="pageNumber">
+                        2
+                    </div>
+                    <div class="pageNumber">
+                        3
+                    </div>
+                </div>
+                <div class="next">
+                    Next
+                </div>
+            </div>
+            <div class="commentContainer">
+                <div class="comment">
+                    <div class="commentAuthorData">
+                        <div class="authorDataElement">
+                            Nome: Fdada
+                        </div>
+                        <div class="authorDataElement">
+                            Matricola: 177843
+                        </div>
+                        <div class="authorDataElement">
+                            Punteggio: dsadsa
+                        </div>
+                        <div class="authorDataElement">
+                            Corso Di Laurea: Ing-Info
+                        </div>
+                    </div>
+                    <div class="commentContent">
+                        <div class="commentTopBar">
+                            <div class="commentTime">
+                                Mar 31,2022
+                            </div>
+                            <div class="commentVoteContainer">
+                                <div class="star">
+                                    yo
+                                </div>
+                                <div class="star">
+                                    yo
+                                </div>
+                                <div class="star">
+                                    yo
+                                </div>
+                                <div class="star">
+                                    yo
+                                </div>
+                                <div class="star">
+                                    yo
+                                </div>
+                            </div>
+                        </div>
+                        <div class="commentText">
+                            CIasdhjaskdhkaj dsajkhdsa jkhd sjk kdsaajkhd sakj dsakjh dsaakj dasskj dsakj daskj fj fdsjh fdkj fdsjak dsajk dsa kjfh
+                        </div>
+                    </div>
+                </div>
+                <div class="comment">
+                    <div class="commentAuthorData">
+                        <div class="authorDataElement">
+                            Nome: Fdada
+                        </div>
+                        <div class="authorDataElement">
+                            Matricola: 177843
+                        </div>
+                        <div class="authorDataElement">
+                            Punteggio: dsadsa
+                        </div>
+                        <div class="authorDataElement">
+                            Corso Di Laurea: Ing-Info
+                        </div>
+                    </div>
+                    <div class="commentContent">
+                        <div class="commentTopBar">
+                            <div class="commentTime">
+                                Mar 31,2022
+                            </div>
+                            <div class="commentVoteContainer">
+                                <div class="star">
+                                    yo
+                                </div>
+                                <div class="star">
+                                    yo
+                                </div>
+                                <div class="star">
+                                    yo
+                                </div>
+                                <div class="star">
+                                    yo
+                                </div>
+                                <div class="star">
+                                    yo
+                                </div>
+                            </div>
+                        </div>
+                        <div class="commentText">
+                            CIasdhjaskdhkaj dsajkhdsa jkhd sjk kdsaajkhd sakj dsakjh dsaakj dasskj dsakj daskj fj fdsjh fdkj fdsjak dsajk dsa kjfh
+                        </div>
+                    </div>
+                </div>
+                <div class="comment">
+                    <div class="commentAuthorData">
+                        <div class="authorDataElement">
+                            Nome: Fdada
+                        </div>
+                        <div class="authorDataElement">
+                            Matricola: 177843
+                        </div>
+                        <div class="authorDataElement">
+                            Punteggio: dsadsa
+                        </div>
+                        <div class="authorDataElement">
+                            Corso Di Laurea: Ing-Info
+                        </div>
+                    </div>
+                    <div class="commentContent">
+                        <div class="commentTopBar">
+                            <div class="commentTime">
+                                Mar 31,2022
+                            </div>
+                            <div class="commentVoteContainer">
+                                <div class="star">
+                                    yo
+                                </div>
+                                <div class="star">
+                                    yo
+                                </div>
+                                <div class="star">
+                                    yo
+                                </div>
+                                <div class="star">
+                                    yo
+                                </div>
+                                <div class="star">
+                                    yo
+                                </div>
+                            </div>
+                        </div>
+                        <div class="commentText">
+                            CIasdhjaskdhkaj dsajkhdsa jkhd sjk kdsaajkhd sakj dsakjh dsaakj dasskj dsakj daskj fj fdsjh fdkj fdsjak dsajk dsa kjfh
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
