87767eb4b8ac429bc857a4eedd83e68a648b7f93
Mattia Aquilina <64703986+Orioncode0@users.noreply.github.com>
2022-08-03 11:19:17 +0200

    Creazione pagina visualizza faq

--- Sito/homepage-faq.php
+++ Sito/homepage-faq.php
@@ -1,3 +1,14 @@
+<?php
+session_start();
+require_once('phpFunctions.php');
+
+if(!isset($_SESSION['loginType']) || (isset($_SESSION['loginType']) && $_SESSION['loginType'] != "Studente"))
+    header('Location: homepage.php');
+
+if(isset($_SESSION['matricola']))
+    $studenteLoggato = getStudenteFromMatricola($_SESSION['matricola']);
+?>
+
 <?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
 	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
@@ -33,58 +44,21 @@
             </form>
                 Logout
         </h2>
+        <div class="vertical-bar"></div>
+            <div class="nav-logo">
+                <a href="homepage-users.php">
+                    <img src="account.png" alt="logo" width="90px">
+                </a>
+            </div>
+        </div>
         </div>
     </div>
     <div class="central-block">
-    <div class="sidebar">
-            <h5>
-                Informazioni
-            </h5>
-            <div style="display:flex;">
-                <img src="arrow.png" alt="duce" width="20px" height="20px" style="display:flex;align-content:center">
-                <h5 style="display: flex;margin:0px;">
-                    <a class="opzionetab" href="homepage-users-Anagrafica.php" style="display: flex;margin:0px;">Visualizza anagrafica</a>
-                </h5>
-            </div>
-            <div style="display:flex;">
-                <img src="arrow.png" alt="duce" width="20px" height="20px" style="display:flex;align-content:center">
-                <h5 style="display: flex;margin:0px;">
-                    <a class="opzionetab" href="homepage-users-Carriera.php" style="display: flex;margin:0px;">Visualizza carriera</a>
-                </h5>
-            </div>
-            <h5>
-                Esami
-            </h5>
-            <div style="display:flex;">
-                <img src="arrow.png" alt="duce" width="20px" height="20px" style="display:flex;align-content:center">
-                <h5 style="display: flex;margin:0px;">
-                    <a class="opzionetab" href="homepage-users-appelliPrenotati.php" style="display: flex;margin:0px;">Appelli prenotati</a>
-                </h5>
-            </div>
-            <div style="display:flex;">
-                <img src="arrow.png" alt="duce" width="20px" height="20px" style="display:flex;align-content:center">
-                <h5 style="display: flex;margin:0px;">
-                    <a class="opzionetab" href="homepage-users-prenotaAppello.php" style="display: flex;margin:0px;">Prenota Appello</a>
-                </h5>
-            </div>
-            <div style="display:flex;">
-                <img src="arrow.png" alt="duce" width="20px" height="20px" style="display:flex;align-content:center">
-                <h5 style="display: flex;margin:0px;">
-                    <a class="opzionetab" href="homepage-users-esamiSostenuti.php" style="display: flex;margin:0px;">Esami Sostenuti</a>
-                </h5>
-            </div>
-            <h5 style="display: flex;margin:0px;">
-                    <a class="opzionetab" href="homepage-bacheca.php" style="display: flex;margin:0px;">Bacheca</a>
-            </h5>
-            <h5 style="display: flex;margin:0px;">
-                    <a class="opzionetab" href="homepage-faq.php" style="display: flex;margin:0px;">FAQ</a>
-            </h5>
-            
-        </div>
+        <?php creaSidebar($_SESSION['loginType']); ?>
         <div class="body">
             <div class="infoTitle">
                 <div class="infoTitle-position">
-                    <h2>Home > Faq</h2><!--Generato dallo script-->
+                    <h2>Home > Bacheche</h2><!--Generato dallo script-->
                 </div>
                 <div class="infoTitle-user">
                     <h2>Nome,Cognome, Matricola</h2><!--Generato dallo script-->
@@ -97,19 +71,23 @@
                         <h2>Nome</h2>
                     </div>
                 </div> 
-                <hr>
-                <div class="listItem">
-                    <div class="element">
-                        <h2>Basi di dati</h2>
-                    </div>
-                    <div class="element">
-                        
-                    </div>
-                    <div class="lastElement">
-                    <img src="arrow.png" alt="err" width="30px" height="30px" style="display:flex;align-content:center">
-                    </div>
-                </div> 
-                <hr>
+                <hr />
+                <form action="homepage-users-visualizzaFaq.php">
+                    <div class="listItem">
+                        <input type="submit" value="" class="bottoneCorsi"> <!--Struttura di ogni bottone -->
+                        <input type="hidden">
+                        <div class="element">
+                            <h2>Basi di dati</h2>
+                        </div>
+                        <div class="element">
+                            
+                        </div>
+                        <div class="lastElement">
+                            <img src="arrowBlack.png" alt="err" width="30px" height="30px" style="display:flex;align-content:center">
+                        </div>
+                    </div> 
+                </form>
+                <hr />
                 <div class="listItem">
                     <div class="element">
                         <h2>Elettronica I</h2>
@@ -118,7 +96,7 @@
                         
                     </div>
                     <div class="lastElement">
-                    <img src="arrow.png" alt="err" width="30px" height="30px" style="display:flex;align-content:center">
+                        <a href="fittizia.php"><img class="arrow" width="30px" height="30px" alt="err" src="arrowBlack.png"></a>
                     </div>
                 </div> 
             <hr>
---
+++ Sito/homepage-users-visualizzaFaq.php
@@ -0,0 +1,373 @@
+<?php
+session_start();
+require_once('phpFunctions.php');
+
+if(!isset($_SESSION['loginType']) || (isset($_SESSION['loginType']) && $_SESSION['loginType'] != "Studente"))
+    header('Location: homepage.php');
+
+if(isset($_SESSION['matricola']))
+    $studenteLoggato = getStudenteFromMatricola($_SESSION['matricola']);
+?>
+
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+
+        <link rel="stylesheet" href="stile-base.css">
+        <link rel="stylesheet" href="stileHomepage-users.css">
+        <link rel="stylesheet" href="stileBacheca.css">
+        <link rel="stylesheet" href="stilePost.css">
+        <link rel="stylesheet" href="stileFaq.css">
+    <title>Homepage</title>
+</head>
+<body>
+    <div class="header">
+        <div class="nav-left">
+            <div class="nav-logo">
+                <a href="homepage.php">
+                    <img src="https://store-images.s-microsoft.com/image/apps.51215.9007199266623456.05e3a154-d5ac-49d8-af6e-ab2f789dc26d.f443b25b-1668-48aa-8137-f8e5609aee45?mode=scale&q=90&h=300&w=300" alt="logo" width="90px">
+                </a>
+            </div>
+            <div class="vertical-bar"></div>
+                <h2>
+                    <form action="">
+                        <input type="button">
+                    </form>
+                    Infostud
+                </h2>
+            <div class="vertical-bar"></div>
+        </div>
+        <div class="nav-right">
+        <h2>
+            <form action="">
+                <input type="button">
+            </form>
+                Logout
+        </h2>
+        <div class="vertical-bar"></div>
+            <div class="nav-logo">
+                <a href="homepage-users.php">
+                    <img src="account.png" alt="logo" width="90px">
+                </a>
+            </div>
+        </div>
+        </div>
+    </div>
+    <div class="central-block">
+        <?php creaSidebar($_SESSION['loginType']); ?>
+        <div class="body">
+            <div class="_body">
+                <div class="infoTitle">
+                    <div class="infoTitle-position">
+                        <h2>Home > Bacheca > Basi di Dati</h2><!--Generato dallo script-->
+                    </div>
+                    <div class="infoTitle-user">
+                        <h2>Nome,Cognome, Matricola</h2><!--Generato dallo script-->
+                    </div>
+                </div>    
+                <hr />
+                <!-- Container delle domande con risposta -->
+                <div class="container">
+                    <div class="title">
+                        Domande e Risposte complete
+                    </div>
+                    <div class="researchBar">
+                        <form action="">
+                            <input type="text">
+                        </form>
+                    </div>
+                    <div class="faqContainer">
+                        <div class="faq">
+                            <div class="upDown">
+                                <form action="">
+                                    <div>
+                                        <img src="up.png" alt="dsa">
+                                        <input type="hidden">
+                                        <input type="button" value="">      
+                                    </div>
+                                </form>
+                                <div>
+                                    0 Voti
+                                </div>
+                                <form action="">
+                                    <div>
+                                        <img src="down.png" alt="dsa">
+                                        <input type="hidden">
+                                        <input type="button" value=""> 
+                                    </div>
+                                </form>
+                            </div>
+                            <div class="vBar"></div>
+                            <div class="faqContent">
+                                <div class="contentRow">
+                                    <div class="contentTitle">
+                                        Domanda
+                                    </div>
+                                    <div class="contentQuestion">
+                                        Serve un libro di testo in particolare per affrontare l'esame
+                                    </div>
+                                </div>
+                                <div class="contentRow">
+                                    <div class="contentTitle">
+                                        Risposta
+                                    </div>
+                                    <div class="content">
+                                        Il libro è un'aggiunta, potrà tranquillamente affrontare l'esame studiando sul materiale checkdate
+                                        metterò a disposizione durante il corso
+                                    </div>
+                                </div>
+                                <div class="faqAuthorData">
+                                    Da david in data 5 dicembre 2020
+                                </div>
+                            </div>
+                        </div>
+                        <div class="faq">
+                            <div class="upDown">
+                                <form action="">
+                                    <div>
+                                        <img src="up.png" alt="dsa">
+                                        <input type="hidden">
+                                        <input type="button" value="">      
+                                    </div>
+                                </form>
+                                <div>
+                                    0 Voti
+                                </div>
+                                <form action="">
+                                    <div>
+                                        <img src="down.png" alt="dsa">
+                                        <input type="hidden">
+                                        <input type="button" value=""> 
+                                    </div>
+                                </form>
+                            </div>
+                            <div class="vBar"></div>
+                            <div class="faqContent">
+                                <div class="contentRow">
+                                    <div class="contentTitle">
+                                        Domanda
+                                    </div>
+                                    <div class="contentQuestion">
+                                        Serve un libro di testo in particolare per affrontare l'esame
+                                    </div>
+                                </div>
+                                <div class="contentRow">
+                                    <div class="contentTitle">
+                                        Risposta
+                                    </div>
+                                    <div class="content">
+                                        Il libro è un'aggiunta, potrà tranquillamente affrontare l'esame studiando sul materiale checkdate
+                                        metterò a disposizione durante il corso
+                                    </div>
+                                </div>
+                                <div class="faqAuthorData">
+                                    Da david in data 5 dicembre 2020
+                                </div>
+                            </div>
+                        </div>
+                        <div class="faq">
+                            <div class="upDown">
+                                <form action="">
+                                    <div>
+                                        <img src="up.png" alt="dsa">
+                                        <input type="hidden">
+                                        <input type="button" value="">      
+                                    </div>
+                                </form>
+                                <div>
+                                    0 Voti
+                                </div>
+                                <form action="">
+                                    <div>
+                                        <img src="down.png" alt="dsa">
+                                        <input type="hidden">
+                                        <input type="button" value=""> 
+                                    </div>
+                                </form>
+                            </div>
+                            <div class="vBar"></div>
+                            <div class="faqContent">
+                                <div class="contentRow">
+                                    <div class="contentTitle">
+                                        Domanda
+                                    </div>
+                                    <div class="contentQuestion">
+                                        Serve un libro di testo in particolare per affrontare l'esame
+                                    </div>
+                                </div>
+                                <div class="contentRow">
+                                    <div class="contentTitle">
+                                        Risposta
+                                    </div>
+                                    <div class="content">
+                                        Il libro è un'aggiunta, potrà tranquillamente affrontare l'esame studiando sul materiale checkdate
+                                        metterò a disposizione durante il corso
+                                    </div>
+                                </div>
+                                <div class="faqAuthorData">
+                                    Da david in data 5 dicembre 2020
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- Container delle domande senza risposta -->
+                <div class="container">
+                    <div class="title">
+                        Domande e Risposte Incomplete
+                    </div>
+                    <div class="researchBar">
+                        <form action="">
+                            <input type="text">
+                        </form>
+                    </div>
+                    <div class="faqContainer">
+                        <div class="faq">
+                            <div class="upDown">
+                                <form action="">
+                                    <div>
+                                        <img src="up.png" alt="dsa">
+                                        <input type="hidden">
+                                        <input type="button" value="">      
+                                    </div>
+                                </form>
+                                <div>
+                                    0 Voti
+                                </div>
+                                <form action="">
+                                    <div>
+                                        <img src="down.png" alt="dsa">
+                                        <input type="hidden">
+                                        <input type="button" value=""> 
+                                    </div>
+                                </form>
+                            </div>
+                            <div class="vBar"></div>
+                            <div class="faqContent">
+                                <div class="contentRow">
+                                    <div class="contentTitle">
+                                        Domanda
+                                    </div>
+                                    <div class="contentQuestion">
+                                        Serve un libro di testo in particolare per affrontare l'esame
+                                    </div>
+                                </div>
+                                <div class="contentRow">
+                                    <div class="contentTitle">
+                                        Risposta
+                                    </div>
+                                    <div class="content">
+                                    </div>
+                                </div>
+                                <div class="faqAuthorData">
+                                    Da david in data 5 dicembre 2020
+                                </div>
+                            </div>
+                        </div>
+                        <div class="faq">
+                            <div class="upDown">
+                                <form action="">
+                                    <div>
+                                        <img src="up.png" alt="dsa">
+                                        <input type="hidden">
+                                        <input type="button" value="">      
+                                    </div>
+                                </form>
+                                <div>
+                                    0 Voti
+                                </div>
+                                <form action="">
+                                    <div>
+                                        <img src="down.png" alt="dsa">
+                                        <input type="hidden">
+                                        <input type="button" value=""> 
+                                    </div>
+                                </form>
+                            </div>
+                            <div class="vBar"></div>
+                            <div class="faqContent">
+                                <div class="contentRow">
+                                    <div class="contentTitle">
+                                        Domanda
+                                    </div>
+                                    <div class="contentQuestion">
+                                        Serve un libro di testo in particolare per affrontare l'esame
+                                    </div>
+                                </div>
+                                <div class="contentRow">
+                                    <div class="contentTitle">
+                                        Risposta
+                                    </div>
+                                    <div class="content">
+                                    </div>
+                                </div>
+                                <div class="faqAuthorData">
+                                    Da david in data 5 dicembre 2020
+                                </div>
+                            </div>
+                        </div>
+                        <div class="faq">
+                            <div class="upDown">
+                                <form action="">
+                                    <div>
+                                        <img src="up.png" alt="dsa">
+                                        <input type="hidden">
+                                        <input type="button" value="">      
+                                    </div>
+                                </form>
+                                <div>
+                                    0 Voti
+                                </div>
+                                <form action="">
+                                    <div>
+                                        <img src="down.png" alt="dsa">
+                                        <input type="hidden">
+                                        <input type="button" value=""> 
+                                    </div>
+                                </form>
+                            </div>
+                            <div class="vBar"></div>
+                            <div class="faqContent">
+                                <div class="contentRow">
+                                    <div class="contentTitle">
+                                        Domanda
+                                    </div>
+                                    <div class="contentQuestion">
+                                        Serve un libro di testo in particolare per affrontare l'esame
+                                    </div>
+                                </div>
+                                <div class="contentRow">
+                                    <div class="contentTitle">
+                                        Risposta
+                                    </div>
+                                    <div class="content">
+                                    </div>
+                                </div>
+                                <div class="faqAuthorData">
+                                    Da david in data 5 dicembre 2020
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- Container della form di input -->
+                <div class="formContainer">
+                    <div class="formBorder">
+                        <div class="formTitle">
+                            Proponi una nuova domanda
+                        </div>
+                        <form action="">
+                            <textarea name="" id="" placeholder="Domanda"></textarea>
+                            <input type="submit">
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
