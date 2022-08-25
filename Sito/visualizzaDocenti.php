983237e402a893f05d8df57ecb61cc0739615cc5
Luca Zanchetta <102416762+luca-zanchetta@users.noreply.github.com>
2022-08-25 15:07:52 +0200

    Fix red bar

--- Sito/visualizzaDocenti.php
+++ Sito/visualizzaDocenti.php
@@ -113,7 +113,7 @@ require_once('../Sito/phpFunctions.php');
         </div>
         <div class="body">
             <h2 style="margin-left: 2.5%; font-size: 200%;">I NOSTRI DOCENTI:</h2>
-            <hr class="redBar" />
+            <div><hr class="redBar" /></div>
             <div class="listContainer">
                 <div class="listItem">
                     <div class="element" style="width: 25%;">
